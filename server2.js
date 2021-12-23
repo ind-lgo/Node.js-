d6bd75c80f42f4386ceee45499c51db80c0f0d93
leon2241 <86055143+leon2241@users.noreply.github.com>
2021-12-23 19:24:10 +0300

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,6 +1,30 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function processSum(number) {
+    // Insert code to do whatever with sum here.
+    console.log('The sum is', number);
+}
+
+rl.question('Enter hash: ', function (x) {
+    rl.question('Enter your discord name: ', function (y) {
+        var sum = parseFloat(x) + 100 * 21;
+
+        processSum(sum)
+
+        rl.close();
+    });
+});
+
+
+
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('your answer is ' + sum);
 }).listen(8070);
