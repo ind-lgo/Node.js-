8688032de3f6fe75f755055f0e4512a751612462
leon2241 <86055143+leon2241@users.noreply.github.com>
2021-12-24 17:46:59 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+function processSum(number) {
+    // Insert code to do whatever with sum here.
+    console.log('The sum is', number);
+
+}
+
+rl.question('Enter hash: ', function (x) {
+    rl.question('Enter your discord name: ', function (y) {
+        var matches = x.match(/(\d+)/);
+        var sum = parseInt(x) + 100 * 21;
+
+        processSum(sum)
+
+        rl.close();
+    });
+});
+
+
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('your answer is ' + sum);
+}).listen(8070);
